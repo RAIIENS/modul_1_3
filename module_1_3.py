f17bd6a4a9a39c918d2efd84c8d9f8b1b1646275
Vladimir N <evaeva6202@gmail.com>
2024-11-18 12:08:15 +0300

    module_1_3
    module_1_3

---
+++ module_1_3.py
@@ -0,0 +1,18 @@
+name = "Vladimir"
+print(name)
+print(name, type(name))
+age = 34
+print(age)
+print(age, type(age))
+age = 34+15
+print(age)
+print(age, type(age))
+is_student = True
+print(is_student)
+print(is_student, type(is_student))
+
+# Ответ
+# Vladimir
+# 34
+# 49
+# True
